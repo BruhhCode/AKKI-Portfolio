2f4b05d6662ffca8895da3c6aa866279ac2a6035
BruhhCode <akshits055@gmail.com>
2026-06-28 17:05:17 +0530

    light rays changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,17 +25,17 @@ export function Hero() {
       <LightRays
         raysOrigin="top-center"
         raysColor="#ffffff"
-        raysSpeed={1}
-        lightSpread={0.5}
+        raysSpeed={0.8}
+        lightSpread={1.6}
         rayLength={3}
         followMouse={true}
         mouseInfluence={0.1}
         noiseAmount={0}
         distortion={0}
-        className="opacity-40"
+        className="custom-rays"
         pulsating={false}
         fadeDistance={1}
-        saturation={1}
+        saturation={1.3}
       />
       <div
         className="pointer-events-none absolute inset-0 opacity-40"
